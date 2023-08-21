480e96f632c28fb4ae42658fabd990513d9a589c
brianjayaandhika <brianjaya.andhika@gmail.com>
2023-08-21 15:55:31 +0700

    changed endpoint

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ db.sync()
 
 app.use('/movies', movieRouter);
 app.use('/user', userRouter);
-app.use('/api', documentation);
+app.use('/', documentation);
 // check emails in https://mailtrap.io/inboxes/2376809/messages/3658600755
 
 app.use(express.static('temp/uploads'));
